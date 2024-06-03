6230de9ef12c8155c77c5ed2c41d4ccea90b636d
Pedro Brun <pedroluccavb@gmail.com>
2024-06-03 10:47:03 -0300

    add consumer that crashes and consumer that throws

---
+++ consumer/consumer-that-throws.ts
@@ -0,0 +1,52 @@
+import { Kafka } from 'kafkajs'
+import { kafkaInstance } from '..'
+import { getTopics } from '../topics'
+import { parseConsumerArguments } from './cli-args'
+import { hideBin } from 'yargs/helpers'
+
+const startConsumerThatThrows = async (kafka: Kafka) => {
+  const args = await parseConsumerArguments(hideBin(process.argv))
+
+  if (!args.topic) {
+    console.log('No topic sent')
+    return
+  }
+  if (!args.group) {
+    console.log('No group sent')
+    return
+  }
+
+  const consumer = kafka.consumer({
+    groupId: args.group,
+    retry: {
+      retries: 3,
+    },
+  })
+
+  const topics = await getTopics(kafka)
+  if (!topics.find((t) => t === args.topic)) {
+    console.log(`Topic ${args.topic} doesnt not exist`)
+    return
+  }
+
+  await consumer.connect()
+
+  console.log('Consumer connected successfully \n')
+  await consumer.subscribe({ topic: args.topic })
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log()
+      console.log('------- CONSUMER THAT CRASHES -------')
+      console.log(
+        `Consuming message from topic ${topic} and partition ${partition}`
+      )
+      console.log(`Message key: ${message.key?.toString()}`)
+      console.log(`Message value: ${message.value?.toString()}`)
+      console.log('Will exit process...')
+      throw new Error('Consumer threw error')
+    },
+  })
+}
+
+startConsumerThatThrows(kafkaInstance)
